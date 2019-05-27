ce83b97ef5101cae0d9d22e3ec3c629c50c7f56e
Lei Jin <i.am.jin.lei@gmail.com>
2019-05-27 16:37:36 +0800

    tmp

--- scripts/ecs.sh
+++ scripts/ecs.sh
@@ -6,7 +6,7 @@ if [ $1 = "up" ] || [ $1 = "down" ] || [ $1 = "del" ] || [ $1 = "desc" ]; then
 	go run $SCRIPT_DIR/../cmd/ecs.go -op=$1
 elif [ $1 = "go" ]; then
 	ip=$(go run $SCRIPT_DIR/../cmd/ecs.go -op=desc | grep Running | cut -d"|" -f5 | xargs)
-    expect -c 'spawn ssh root@'"$ip"'; expect "assword:"; send "'"$ECS_ROOT_PWD"'\r"; interact'
+    expect -c 'spawn ssh -o StrictHostKeyChecking=no root@'"$ip"'; expect "assword:"; send "'"$ECS_ROOT_PWD"'\r"; interact'
 else
 	echo -e "supported commands are: up, down, del, desc\n"
 fi
